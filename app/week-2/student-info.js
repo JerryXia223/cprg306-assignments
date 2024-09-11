bf7fd5978a92be4234e0ac03a10ea15031034880
JerryXia223 <jerry.xia@edu.sait.ca>
2024-09-11 17:42:58 -0600

    week 2

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,13 +2,13 @@ import Link from "next/link"
 
 export default function Page() {
   return (
-    <div>
-      <h1>Jerry Xia</h1>
+    <main>
+      <p>Jerry Xia</p>
       <ul>
         <li>
           <Link href="https://github.com/JerryXia223/cprg306-assignments">Github repository</Link>
         </li>
       </ul>
-    </div>
+    </main>
   );
 };
